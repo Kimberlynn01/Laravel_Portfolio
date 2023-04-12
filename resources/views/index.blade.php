1563dd89a873c9ded3ff5b645778891b4a8a12e7
Danudiraja <dirajadanu@gmail.com>
2023-04-12 09:14:41 +0700

    Replace Design

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,14 +3,27 @@
     <!-- Main Content </> -->
     @section('content')
     <section class="content">
-        <div class="container-fluid">
+        <div class="container-fluid container-content">
             <div class="row">
-                <div class="col d-flex justify-content-start">
+                {{-- <div class="col d-flex justify-content-start">
                     <div class="body-content">
-                        <h2 class="title-content">I’m DanuDiraja Soenoto</h2>
+                        <img src="img/Hello.png" alt="">
+                        <h2 class="title-content">DanuDiraja Soenoto</h2>
                         <p class="text-content">Hello Everyone, Introduction My Name Is DanuDiraja Soenoto
                             <br> I am Frontend Devlopment And UI/UX
                         </p>
+                        <button class="hire-content">Hire Me</button>
+                        <button class="cv-content">Download CV</button>
+                    </div>
+                </div> --}}
+                <div class="col d-flex justify-content-start">
+                    <div class="body-content">
+                        <img src="/img/hello.png" alt="" class="hi-content">
+                        <p class="hello-content">Hello</p>
+                        <h2 class="title-content">I'm</h2>
+                        <h2 class="title-content"><label>Danu</label>Diraja Soenoto</h2>
+                        <p class="text-content">Someone Graphic Designer UI/UX <br>
+                            And Front End Devloper...</p>
                         <button class="hire-content">Hire Me</button>
                         <button class="cv-content">Download CV</button>
                     </div>
